b34ed34f10e4a71a0dfb02b6ad2374db3a8e3a85
Tam Nguyen <nguyentamvinhlong@gmail.com>
2014-12-04 23:50:09 +0700

    init project media analytic data online

---
+++ app/database/migrations/2014_12_01_173154_create_mediaview_category.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateMediaviewCategory extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('mediaview_category', function (Blueprint $table) {
+				$table->increments('id');
+				$table->string('name')->nullable();
+				$table->string('slug')->nullable();
+				$table->timestamps();
+				$table->string('user_role')->nullable();
+			});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::table('mediaview_category', function (Blueprint $table) {
+				//
+			});
+	}
+
+}
---
+++ app/views/admin/users/monitor.blade.php
@@ -0,0 +1,3 @@
+<h1>Monitoring users</h1>
+<h3>{{ $title }}</h3>
+<p>{{ $description }}</p>
