c5a01ba807274c04853705e89c2253784056ac12
Tam Nguyen <nguyentamvinhlong@gmail.com>
2014-12-08 11:45:21 +0700

    update core

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql'      => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'laravel',
+			'database'  => 'mediaview',
 			'username'  => 'root',
 			'password'  => 'nguyen',
 			'charset'   => 'utf8',
