769d31fb33fdb5a573ff63f598ab290008944262
Tam Nguyen <nguyentamvinhlong@gmail.com>
2014-12-18 01:01:24 +0700

    update

--- app/lang/en/media/table.php
+++ app/lang/en/media/table.php
@@ -10,7 +10,7 @@ return array(
 	'slug'             => "Slug",
 	'options'          => "Options",
 	'main_cat'         => 'Main Cat',
-	'company_brand'    => 'Company brand',
-	'sub_cat_main_ind' => 'Sub cat main ind',
-	'sub_ind_headline' => 'Sub ind headline',
+	'company_brand'    => 'Company',
+	'sub_cat_main_ind' => 'Sub cat',
+	'sub_ind_headline' => 'Headline',
 );
